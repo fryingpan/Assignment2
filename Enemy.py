13299b6f58d670519e789682a9e06d53204fa1dd
Carla Castro <ccastro6@jhu.edu>
2014-09-21 00:22:04 -0400

    added enemies, made them face opp dir, added random motion

--- Enemy.py
+++ Enemy.py
@@ -1,26 +1,99 @@
 # Assignment 2
 
-
+import random
 import pygame
 import sys
+import math
 
 class Enemy(pygame.sprite.Sprite):
+    IMAGE_UP = None
+    IMAGE_DOWN = None
+    IMAGE_RIGHT = None
+    IMAGE_LEFT = None
 
-    def __init__(self):
+    def __init__(self, screen):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("fbwall.png").convert()
-        self.image_rect = self.image.get_rect()
-        self.x = 100
-        self.y = 100
+        if not Enemy.IMAGE_UP and not Enemy.IMAGE_DOWN and not Enemy.IMAGE_LEFT and not Enemy.IMAGE_RIGHT:
+            Enemy.IMAGE_UP = pygame.image.load("FPGraphics/Food/IceCreamBack.png").convert_alpha()
+            Enemy.IMAGE_DOWN = pygame.image.load("FPGraphics/Food/IceCreamFront.png").convert_alpha()
+            Enemy.IMAGE_LEFT = pygame.image.load("FPGraphics/Food/IceCreamLeft.png").convert_alpha()
+            Enemy.IMAGE_RIGHT = pygame.image.load("FPGraphics/Food/IceCreamRight.png").convert_alpha()
+        self.image_up = Enemy.IMAGE_UP
+        self.image_down = Enemy.IMAGE_DOWN
+        self.image_right = Enemy.IMAGE_RIGHT
+        self.image_left = Enemy.IMAGE_LEFT
+        self.image = None
 
-    def draw(self, surface):
+        self.screen = screen
+        self.swidth = screen.get_width()
+        self.sheight = screen.get_height()
+        self.x = random.randint(0, self.swidth - self.image_left.get_width())
+        self.y = random.randint(0, self.sheight - self.image_up.get_height())
+
+        self.speed = 1
+        self.direction = random.randint(0, 3)
+        self.angle = random.randint(0, 360) * (math.pi/180)
+        self.face = 'u' 
+
+
+    def update(self):
+        self.move()
+        #check that the new movement is within the boundaries
+        if self.check_collide() is True:
+            # print "COLLIDED"
+            self.direction = random.randint(0, 3)
+            self.angle = random.randint(0, 360) * (math.pi/180) 
+
+            print "RANDINT" + str(self.direction)
+            self.move()
+        # print "THE X COMPONENT: " + str(self.x)
+        # print "THE Y COMPONENT: " + str(self.y)
+        self.draw()
+
+
+    def move(self):
+        dist = self.speed
+        if self.direction == 0:
+            self.x += dist * math.sin(self.angle)
+        elif self.direction == 1:
+            self.x -= dist * math.sin(self.angle)
+        elif self.direction == 2:
+            self.y += dist * math.cos(self.angle)
+        elif self.direction == 3:
+            self.y -= dist * math.cos(self.angle)
+
+    def set_face(self, player_face):
+        if player_face == 'u':
+            self.image = self.image_down
+        elif player_face == 'd':
+            self.image = self.image_up
+        elif player_face == 'l':
+            self.image = self.image_right
+        elif player_face == 'r':
+            self.image = self.image_left
+
+    def draw(self):
         """ Draw on surface """
         # blit yourself at your current position
-        surface.blit(self.image, (self.x, self.y))
-        
-pygame.init()        
-        
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def check_collide(self):
+        collide = False
+        if self.x < 0:
+            self.x = 0
+            collide = True
+        elif self.x > (self.swidth - self.image_right.get_width()):
+            self.x = self.swidth - self.image_right.get_width()
+            Collide = True
+        if self.y < 0:
+            self.y = 0
+            collide = True
+        elif self.y > (self.sheight - self.image_down.get_height()):
+            self.y = (self.sheight - self.image_down.get_height())
+            collide = True
+        return collide
+                
         
     
     
--- Setup.py
+++ Setup.py
@@ -15,18 +15,28 @@ except ImportError, err:
 
 class Game(object):
 
-    def __init__(self):
+    def __init__(self, num_enemies):
     
         pygame.init()
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((800, 600))
+        self.screen_rect = self.screen.get_rect()
         pygame.display.set_caption("Master Chef's wicked adventure with his ice cream buddies")
         
         #Initialize objects on screen----------------
         self.character = Player.Player()
         # bad = Enemy()
-        self.charrect = self.character.image.get_rect()
+        self.charrect = self.character.image_rect
         # badrect = bad.image.get_rect()
+
+        #create enemy group 
+        self.enemy_list = pygame.sprite.Group()
+
+        #add all the enemies to the list of enemies
+        for e in range(num_enemies):  
+            enemy = Enemy.Enemy(self.screen)
+            self.enemy_list.add(enemy)
+
 
         #I don't actually know what this does
         pygame.event.set_allowed([QUIT, KEYDOWN])
@@ -37,15 +47,19 @@ class Game(object):
             self.clock.tick()
             running = self.handleEvents()
             #Key Handling----------------------------
-            
+
             self.character.handle_keys() # handle the keys
             #----------------------------------------
-
             #Display Handling------------------------
             self.screen.fill((255,255,255)) # fill the screen with white
+            #move and draw the enemies
+            player_face = self.character.get_face()
+            for enemy in self.enemy_list.sprites():
+                enemy.set_face(player_face)
+                enemy.update()
+
             self.character.draw(self.screen) # draw the bird to the screen
-            i = 0
-            j = 0
+            
             pygame.display.update() # update the screen
             
     def handleEvents(self):
@@ -62,6 +76,7 @@ class Game(object):
                   
 # create a game and run it
 if __name__ == '__main__':
-    game = Game()
+    num_enemies = 1
+    game = Game(num_enemies)
     game.run()
 
