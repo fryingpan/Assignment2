6daf8d65231c3a1e9afc954fa43f0af89447ac37
CarlaCastro <ccastro6@jhu.edu>
2014-10-02 21:14:29 -0400

    Added block sprites

--- states/drawing.py
+++ states/drawing.py
@@ -11,149 +11,172 @@ import pygame.color as PC
 import pygame.mixer as PX
 
 class Globals(object):
-    SCREEN = None #Surface
-    SCREENSURFACE = None
-    OPTIONS = None #Options list from options.txt
-    WIDTH = None
-    HEIGHT = None
-    STATE = None
-    RUNNING = None
-    OBJECTS = {'None': None }
-    GRID = {'Null':0}
-    INITIALGRID = {'Null':0}
+	SCREEN = None #Surface
+	SCREENSURFACE = None
+	OPTIONS = None #Options list from options.txt
+	WIDTH = None
+	HEIGHT = None
+	STATE = None
+	RUNNING = None
+	OBJECTS = {'None': None }
+	GRID = {'Null':0}
+	INITIALGRID = {'Null':0}
+
+#Block sprite class
+class Block(PG.sprite.Sprite):
+	def __init__(self, color, rect):
+		PG.sprite.Sprite.__init__(self)
+		self.image = PG.surface([rect.width, rect.height])
+		self.image.fill(color)
+		self.rect = rect
+
+	def draw_block(self):
+		Globals.SCREEN.blit(self.image, self.rect)
 
 
 
 def draw_text_box(text, textcolor, boxcolor, x1, x2, y1, y2, ref):
 
-    rect = PG.rect.Rect(x1,y1,(x2-x1),(y2-y1))
-    surf = PG.Surface([rect.width,rect.height])
-    surf.fill(boxcolor)
-    Globals.SCREEN.blit(surf,rect)
-    surf = Locals.FONT.render(text, True, textcolor)
-    Globals.SCREEN.blit(surf, rect)
-    Globals.OBJECTS[ref] = rect
-    
-    
+	rect = PG.rect.Rect(x1,y1,(x2-x1),(y2-y1))
+	surf = PG.Surface([rect.width,rect.height])
+	surf.fill(boxcolor)
+	Globals.SCREEN.blit(surf,rect)
+	surf = Locals.FONT.render(text, True, textcolor)
+	Globals.SCREEN.blit(surf, rect)
+	Globals.OBJECTS[ref] = rect
+	
+	
 def draw_box(color,x1,x2,y1,y2,ref):
 
-    rect = PG.rect.Rect(x1,y1,(x2-x1),(y2-y1))
-    surf = PG.Surface([rect.width,rect.height])
-    surf.fill(color)
-    Globals.SCREEN.blit(surf,rect)
-    Globals.OBJECTS[ref] = rect
+	rect = PG.rect.Rect(x1,y1,(x2-x1),(y2-y1))
+	surf = PG.Surface([rect.width,rect.height])
+	surf.fill(color)
+	Globals.SCREEN.blit(surf,rect)
+	Globals.OBJECTS[ref] = rect
 def draw_rect_box(color,rect):
 
-    surf = PG.Surface([rect.width,rect.height])
-    surf.fill(color)
-    Globals.SCREEN.blit(surf,rect)
-    
-
-
-
-    
+	surf = PG.Surface([rect.width,rect.height])
+	surf.fill(color)
+	Globals.SCREEN.blit(surf,rect)
+	
+	
 def draw_map(mapfile):
-    f = open(mapfile,'r')
-    mapLines = list(f)
-    f.close()
-    
-    
+	#group of block sprites
+	block_group = PG.sprite.Group()
 
-    Globals.SCREEN = PDI.get_surface()
+	f = open(mapfile,'r')
+	mapLines = list(f)
+	f.close()
+	
+	
 
-    
-    
-    listLines = list()
-    y=0
-    for item in mapLines:
-        y = y+1
-        item=item.strip('n')
-        item=item.strip('/')
-        charList=list(item)
-        listLines.append(charList)
-        
+	Globals.SCREEN = PDI.get_surface()
 
-    x = len(listLines[0])
+	
+	
+	listLines = list()
+	y=0
+	for item in mapLines:
+		y = y+1
+		item=item.strip('n')
+		item=item.strip('/')
+		charList=list(item)
+		listLines.append(charList)
+		
+
+	x = len(listLines[0])
  
-    f = open('options.txt', 'r')
-    Globals.OPTIONS = list(f)
-    for item in Globals.OPTIONS:
-        strList=item.split("= ")
-        if strList[0] != "\n":
-            o = strList[0]
-        v = strList[1]
-        v = v.rstrip()
-        v = v.rstrip('n')
-        v = v.rstrip('/')
-       
-        if o == 'WIDTH':
-            Globals.WIDTH = int(v)
-        elif o == 'HEIGHT':
-            Globals.HEIGHT = int(v)
+	f = open('options.txt', 'r')
+	Globals.OPTIONS = list(f)
+	for item in Globals.OPTIONS:
+		strList=item.split("= ")
+		if strList[0] != "\n":
+			o = strList[0]
+		v = strList[1]
+		v = v.rstrip()
+		v = v.rstrip('n')
+		v = v.rstrip('/')
+	   
+		if o == 'WIDTH':
+			Globals.WIDTH = int(v)
+		elif o == 'HEIGHT':
+			Globals.HEIGHT = int(v)
 
 
 
-    ###WRONG MATH HERE, NEEDS FIXING### figure out how to import screen dimensions from masterstate
-    print x
-    rectHeight=80
-    print y
-    rectWidth=60
-    
-    rectLeft = 0
-    for z in range(0,x-1):
-        
-        rectTop = 0   
-        for q in range(0, y):
-            
-            
-            zS=str(z)
-            
-            qS=str(q)
-           
-            newRect = PG.Rect(rectTop,rectLeft,rectHeight,rectWidth)
-            Globals.GRID[zS+qS] = newRect
-            rectTop = rectTop + rectHeight
-           
-            Globals.INITIALGRID[zS+qS] = listLines[z][q]
-            draw_map_objects(listLines[z][q],zS,qS)
-            
+	###WRONG MATH HERE, NEEDS FIXING### figure out how to import screen dimensions from masterstate
+	print x
+	rectHeight=80
+	print y
+	rectWidth=60
+	
+	rectLeft = 0
+	for z in range(0,x-1):
+		
+		rectTop = 0   
+		for q in range(0, y):
+			
+			
+			zS=str(z)
+			
+			qS=str(q)
+		   
+			newRect = PG.Rect(rectTop,rectLeft,rectHeight,rectWidth)
+			Globals.GRID[zS+qS] = newRect
+			rectTop = rectTop + rectHeight
+		   
+			Globals.INITIALGRID[zS+qS] = listLines[z][q]
+			# draw_map_objects(listLines[z][q],zS,qS)
+
+			#create Block sprites
+			if char == '#':
+				new_block = create_Block(PC.Color('blue'), Globals.Grid[zS+qS])
+			elif char == 'S':
+				new_block = create_Block(PC.Color('red'),Globals.GRID[zS+qS])
+
+			new_block.draw_block()
+			#add new block to the block group
+			block_group.add(new_block)
 
 
-            
-        rectLeft = rectLeft + rectWidth
+		rectLeft = rectLeft + rectWidth
   #At this point the grid is a dict of rects, 00 being the first block, 10 being to the right, and 01 being down
+
+  	#return the group of sprites created
+  	return block_group
 
 
 def draw_map_objects(char,zS,qS):
-    if char == '#':
-        draw_rect_box(PC.Color('blue'),Globals.GRID[zS+qS])
-    elif char == 'S':
-        draw_rect_box(PC.Color('red'),Globals.GRID[zS+qS])
+	if char == '#':
+		draw_rect_box(PC.Color('blue'),Globals.GRID[zS+qS])
+	elif char == 'S':
+		draw_rect_box(PC.Color('red'),Globals.GRID[zS+qS])
 
-    else:
-        return False
+	else:
+		return False
 
-
-
-
-    
+#creates a new block sprite
+def create_Block(color, rect):
+	new_block = Block(color, rect)
+	return new_block
+	
 def main():
-    PG.init() #Initialize pygame
-    PDI.init() #Initialize display
-    Globals.SCREEN = PDI.set_mode((1000, 1000), PG.DOUBLEBUF|PG.HWSURFACE)
-    SCREENSURFACE = PDI.get_surface() #Sets SCREEN
-    Globals.SCREEN.fill(PC.Color('white'))
-    PDI.flip()
+	PG.init() #Initialize pygame
+	PDI.init() #Initialize display
+	Globals.SCREEN = PDI.set_mode((1000, 1000), PG.DOUBLEBUF|PG.HWSURFACE)
+	SCREENSURFACE = PDI.get_surface() #Sets SCREEN
+	Globals.SCREEN.fill(PC.Color('white'))
+	PDI.flip()
 
-    draw_map('mapfile.txt')
-    PDI.flip()
-    print Globals.GRID
-    print Globals.INITIALGRID
-    
+	draw_map('mapfile.txt')
+	PDI.flip()
+	print Globals.GRID
+	print Globals.INITIALGRID
+	
 
 
 
 
 
 if __name__ == "__main__":
-    main()
+	main()
