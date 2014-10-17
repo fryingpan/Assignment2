b59d47d96ec0666d1df9722356462cf17c2c4c98
mymaryyn@gmail.com <mymaryyn@gmail.com>
2014-10-17 17:55:37 -0400

    indentation errors fixed

--- highscores.py
+++ highscores.py
@@ -29,106 +29,104 @@ class Locals(object):
 #All this below is from before
 class HighScores:
 
-        def __init__(self):
-                PG.font.init()
+    def __init__(self):
+        PG.font.init()
 
 
-                Globals.SCREEN = PDI.set_mode((800, 600), PG.DOUBLEBUF|PG.HWSURFACE)
-                Globals.WIDTH = Globals.SCREEN.get_width()
-                Globals.HEIGHT = Globals.SCREEN.get_height()
+        Globals.SCREEN = PDI.set_mode((800, 600), PG.DOUBLEBUF|PG.HWSURFACE)
+        Globals.WIDTH = Globals.SCREEN.get_width()
+        Globals.HEIGHT = Globals.SCREEN.get_height()
 
-                self.color = PC.Color("black")
-                self.time = 0.0
-                Globals.SCREEN.fill(PC.Color("black"))
-                self.text_surface = self.get_text_surface()
+        self.color = PC.Color("black")
+        self.time = 0.0
+        Globals.SCREEN.fill(PC.Color("black"))
+        self.text_surface = self.get_text_surface()
 
-        def render(self):
-                width, height = self.text_surface.get_size()
-                Globals.SCREEN.blit(self.text_surface, (Globals.WIDTH/2 - width/2, Globals.HEIGHT/2 - height/2))
+    def render(self):
+        width, height = self.text_surface.get_size()
+        Globals.SCREEN.blit(self.text_surface, (Globals.WIDTH/2 - width/2, Globals.HEIGHT/2 - height/2))
 
-        def update(self, time):
-                self.time += time
-                fadein = 2.0
-                if self.time < fadein:
-                        ratio = self.time / fadein
-                        value = int(ratio * 255)
-                        self.color = PC.Color(value, value, value)
+    def update(self, time):
+        self.time += time
+        fadein = 2.0
+        if self.time < fadein:
+            ratio = self.time / fadein
+            value = int(ratio * 255)
+            self.color = PC.Color(value, value, value)
 
-        def event(self, event):
-                fadeout = 0.2
-                if event.type == PG.KEYDOWN and event.key == PG.K_ESCAPE:
-                        Globals.RUNNING = False
-                elif event.type == PG.KEYDOWN and event.key == PG.K_SPACE:
-                        Locals.CHANGESTATE = "Menu"
+    def event(self, event):
+        fadeout = 0.2
+        if event.type == PG.KEYDOWN and event.key == PG.K_ESCAPE:
+                Globals.RUNNING = False
+        elif event.type == PG.KEYDOWN and event.key == PG.K_SPACE:
+                Locals.CHANGESTATE = "Menu"
 
-        def addScoretoText(): #only run if SCORE !=0
-                f = open('scores.txt', 'a')
-                scoretoAdd = str(SCORE)
-                toAdd = "Player " + scoretoAdd
-                f.write(toAdd)
-                f.close()
+    def addScoretoText(): #only run if SCORE !=0
+        f = open('scores.txt', 'a')
+        scoretoAdd = str(SCORE)
+        toAdd = "Player " + scoretoAdd
+        f.write(toAdd)
+        f.close()
 
 
-        def get_text_surface(self):
-                rect = Rect(100, 100, 300, 300)
-                surface = PG.Surface(rect.size)
-                text_color = PC.Color("white")
-                font = PG.font.Font(None, 30)
-                surface.fill(self.color)
-                with open('scores.txt', 'r') as f:
-                    scores = f.read()
-                    surface = render_text(scores, font, rect, text_color, self.color)
-                return surface
+    def get_text_surface(self):
+        rect = Rect(100, 100, 300, 300)
+        surface = PG.Surface(rect.size)
+        text_color = PC.Color("white")
+        font = PG.font.Font(None, 30)
+        surface.fill(self.color)
+        with open('scores.txt', 'r') as f:
+            scores = f.read()
+            surface = render_text(scores, font, rect, text_color, self.color)
+        return surface
 
 def initialize():
-        Locals.STATE = HighScores()
-        Locals.CHANGESTATE = 'Scores'
+    Locals.STATE = HighScores()
+    Locals.CHANGESTATE = 'Scores'
 
 def run(elapsed, event):
-        Locals.STATE.render()
-        PDI.flip()
-        Locals.STATE.update(elapsed)
+    Locals.STATE.render()
+    PDI.flip()
+    Locals.STATE.update(elapsed)
 
-        if SCORE != 0:
-
-
+    if SCORE != 0:
         for event in PE.get():
-                print(PG.QUIT)
-                if event.type == PG.QUIT:
-                        return False
-                else:
-                        if(Locals.STATE.event(event) == False):
-                                return False
+            print(PG.QUIT)
+            if event.type == PG.QUIT:
+                return False
+            else:
+                if(Locals.STATE.event(event) == False):
+                    return False
 
 
 
 def render_text(string, font, rect, text_color, background_color):
-        final_lines = []
+    final_lines = []
 
-        lines = string.splitlines()
+    lines = string.splitlines()
 
-        # Create a series of lines that will fit in rect
-        for line in lines:
-                if font.size(line)[0] > rect.width:
-                        words = line.split(' ')
-                        # if the name and score is longer than the width of the surface
-                        for word in words:
-                                if font.size(word)[0] >= rect.width:
-                                        raise TextRectException, "The word " + word + " is too long to fit in the rect passed."
-                else:
-                        final_lines.append(line)
+    # Create a series of lines that will fit in rect
+    for line in lines:
+        if font.size(line)[0] > rect.width:
+            words = line.split(' ')
+            # if the name and score is longer than the width of the surface
+            for word in words:
+                if font.size(word)[0] >= rect.width:
+                    raise TextRectException, "The word " + word + " is too long to fit in the rect passed."
+            else:
+                final_lines.append(line)
 
-        # Write onto the surface
-        surface = PG.Surface(rect.size)
-        surface.fill(background_color)
+    # Write onto the surface
+    surface = PG.Surface(rect.size)
+    surface.fill(background_color)
 
-        accumulated_height = 0
-        for line in final_lines:
-                if line != "":
-                        tempsurface = font.render(line, 1, text_color)
-                        surface.blit(tempsurface, ((rect.width - tempsurface.get_width()) / 2, accumulated_height))
-                accumulated_height += font.size(line)[1]
+    accumulated_height = 0
+    for line in final_lines:
+        if line != "":
+            tempsurface = font.render(line, 1, text_color)
+            surface.blit(tempsurface, ((rect.width - tempsurface.get_width()) / 2, accumulated_height))
+        accumulated_height += font.size(line)[1]
 
-        return surface
+    return surface
 
 
